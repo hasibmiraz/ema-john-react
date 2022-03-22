7c43ad8275ddd47200e8be62dafe739bd6fee955
hasibmiraz <hasibmiraz09@gmail.com>
2022-03-23 02:04:26 +0600

    Product card template complete

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,12 +2,24 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-  const { name, price, ratings, seller, stock, img } = props.product;
+  const { name, price, ratings, seller, img } = props.product;
   console.log(props.product);
   return (
     <div className="product">
       <img src={img} alt={name} />
-      <p className="product-name">{name}</p>
+      <div className="product-decription">
+        <p className="product-name">{name}</p>
+        <p>Price: ${price}</p>
+        <p>
+          <small>Manufacturer: {seller}</small>
+        </p>
+        <p>
+          <small>Rating: {ratings} Stars</small>
+        </p>
+      </div>
+      <button className="btn-cart">
+        <p>Add to cart</p>
+      </button>
     </div>
   );
 };
